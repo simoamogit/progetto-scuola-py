e485129cd258d275405e37215df5ad8b77b144e8
simoamogit <simoneinver@gmail.com>
2025-03-14 19:10:19 +0100

    Add files via upload

--- app.py
+++ app.py
@@ -142,5 +142,5 @@ scheduler.add_job(func=invia_notifiche, trigger="interval", hours=1)
 scheduler.start()
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT") or 5000)
     app.run(host='0.0.0.0', port=port)
